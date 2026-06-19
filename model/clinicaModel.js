538ed688fc355f88a26be1d3aff77985f534ee8f
Carlos0805 <empanadas905@gmail.com>
2026-06-18 20:51:02 -0400

    cambios

--- model/clinicaModel.js
+++ model/clinicaModel.js
@@ -2,19 +2,16 @@ const db = require('../config/db');
 
 const ClinicaModel = {
 
-    // selecciona a todos los pacientes
     getAll: async () => {
         const [rows] = await db.execute('SELECT * FROM pacientes');
         return rows;
     },
 
-    // selecciona a un paciente por ID
     getById: async (id) => {
         const [rows] = await db.execute('SELECT * FROM pacientes WHERE id = ?', [id]);
         return rows[0];
     },
 
-    // Crear un nuevo paciente
     create: async (paciente) => {
         const { nombre, rut, edad, prevision } = paciente;
         const [result] = await db.execute(
@@ -24,7 +21,6 @@ const ClinicaModel = {
         return result.insertId;
     },
 
-    // Actualizar un paciente existente
     update: async (id, paciente) => {
         const { nombre, rut, edad, prevision } = paciente;
         const [result] = await db.execute(
@@ -34,7 +30,6 @@ const ClinicaModel = {
         return result.affectedRows;
     },
 
-    // Eliminar un paciente
     delete: async (id) => {
         const [result] = await db.execute('SELECT * FROM pacientes WHERE id = ?', [id]);
         if (result.length === 0) return 0;
